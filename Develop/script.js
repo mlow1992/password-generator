6a5db805941070fd61c19a87cd9ae17d5ddc24f2
General Zelgius <mattlow123@gmail.com>
2022-01-22 12:01:58 -0500

    added questions

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,25 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+   var char = prompt("How many characters would you like?");
+
+    if (char < 8) {
+      alert("You need at least 8 characters!"); 
+    }
+    else if (char > 128) {
+      alert("Too many characters!");
+      
+    }
+    
+    var confirmLC = confirm("Would you like lowercase letters?");
+
+    var confirmUC = confirm("Would you like uppercase letters?");
+
+    var confirmNum = confirm("Would you like numbers?");
+
+    var confirmSpec = confirm("Would you like special characters?")
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
